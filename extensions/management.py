72e7f9ec72d122432040216457e3e6101ecb7a5e
Pierre 'DrumSlayer' SARRET <drumslayer@drumslayer.ovh>
2021-11-24 20:50:06 +0100

    New meeting channels names are now iterated following current user's category meeting channels

--- extensions/management.py
+++ extensions/management.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 from discord.ext import commands
 
 from utils.discord import is_team_allowed
@@ -16,7 +18,7 @@ class TaskCog(commands.Cog):
     # Meeting voice channel creation & deletion listener
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        # Create voice channel
+        # Create voice channel block
         if (
             after.channel
             and after.channel.name
@@ -25,22 +27,36 @@ class TaskCog(commands.Cog):
         ):
             list_channels_name = []
 
+            # List meeting channels already existing in the user's category
             for channel in after.channel.category.voice_channels:
                 if channel.name.startswith(
                     self.bot.config["voice_channel_creation"]["new_channel_name"]
                 ):
                     list_channels_name.insert(len(list_channels_name), channel.name)
-            # temp debug
-            # print(list_channels_name)
 
-            newchannel = await member.guild.create_voice_channel(
-                self.bot.config["voice_channel_creation"]["new_channel_name"],
+            # Iterate through the existing channels (if they exist) to create an non-existing one
+            for i in count(1):
+                if (
+                    not self.bot.config["voice_channel_creation"]["new_channel_name"]
+                    + " #"
+                    + str(i)
+                    in list_channels_name
+                    or not list_channels_name
+                ):
+                    new_channel_number = str(i)
+                    break
+
+            # Create the channel and move member
+            new_channel = await member.guild.create_voice_channel(
+                self.bot.config["voice_channel_creation"]["new_channel_name"]
+                + " #"
+                + new_channel_number,
                 overwrites=None,
                 category=after.channel.category,
             )
-            await member.move_to(newchannel)
+            await member.move_to(new_channel)
 
-        # Delete voice channel
+        # Delete voice channel block
         if (
             before.channel
             and before.channel.name.startswith(
